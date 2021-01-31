cbf6662341c796e74eda995262e0f28f9cd6aa06
Jude Kajura <kbjude@gmail.com>
2021-01-31 12:42:58 +0300

    WIP display of first buttton

---
+++ src/components/ButtonPanel.js
@@ -0,0 +1,10 @@
+import React from "react";
+export default function buttonPanel(name){
+    return (
+        <div>
+            <button>
+                {name}
+            </button>
+        </div>
+    )
+}
