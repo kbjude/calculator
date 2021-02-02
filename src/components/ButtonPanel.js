a9be3c895b5e4baf6dab4d7d1a3b024f9ea8603f
Jude Kajura <kbjude@gmail.com>
2021-02-02 19:30:38 +0300

    Adding other rows

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -2,40 +2,84 @@ import React from "react";
 
 export default function ButtonPanel({
     ac, addition, subraction, modulus, division,
-    seven, eight, nine, multiple
+    seven, eight, nine, multiple,
+    four, five, six, minus,
+    one, two, three, plus,
+    zero, dot, equals
 
 }) {
     return (
         <div>
-            <button>
-                {ac}
-            </button>
-            <button>
-                {addition}
-            </button>
-            <button>
-                {subraction}
-            </button>
-            <button>
-                {modulus}
-            </button>
-            <button>
-                {division}
-            </button>
-        </div>
-        <div>
-            <button>
-                {seven}
-            </button>
-            <button>
-                {eight}
-            </button>
-            <button>
-                {nine}
-            </button>
-            <button>
-                {multiple}
-            </button>
+            <div>
+                <button>
+                    {ac}
+                </button>
+                <button>
+                    {addition}
+                </button>
+                <button>
+                    {subraction}
+                </button>
+                <button>
+                    {modulus}
+                </button>
+                <button>
+                    {division}
+                </button>
+            </div>
+            <div>
+                <button>
+                    {seven}
+                </button>
+                <button>
+                    {eight}
+                </button>
+                <button>
+                    {nine}
+                </button>
+                <button>
+                    {multiple}
+                </button>
+            </div>
+            <div>
+                <button>
+                    {four}
+                </button>
+                <button>
+                    {five}
+                </button>
+                <button>
+                    {six}
+                </button>
+                <button>
+                    {minus}
+                </button>
+            </div>
+            <div>
+                <button>
+                    {one}
+                </button>
+                <button>
+                    {two}
+                </button>
+                <button>
+                    {three}
+                </button>
+                <button>
+                    {plus}
+                </button>
+            </div>
+            <div>
+                <button>
+                    {zero}
+                </button>
+                <button>
+                    {dot}
+                </button>
+                <button>
+                    {equals}
+                </button>
+            </div>
         </div>
     )
 }
