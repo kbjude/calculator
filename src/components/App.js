40cdf6a4c91092a0145244529c3b909632b3a3de
Jude Kajura <kbjude@gmail.com>
2021-02-02 19:48:50 +0300

    Displaying the buttons

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,9 @@ import ButtonPanel from './ButtonPanel.js';
 
 const App = () => {
   return (
-    <ButtonPanel ac="AC" addition="+" subraction="-" modulus="%" division="/" seven="7" eight="8" nine="9" multiple="X"/>
+    <ButtonPanel ac="AC" addition="+" subraction="-" modulus="%" division="/" seven="7" eight="8" nine="9" multiple="X"/>,
+    <ButtonPanel     seven="7" eight="8" nine="9" multiple="X" four="4" five="5" six="6" minus="-" one="1" two="2" three="3" plus="+" />
+
 
   );
 }
