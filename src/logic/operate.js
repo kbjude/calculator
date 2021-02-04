01d986550121dd6b869ae2ab9fd811b152705b14
Jude Kajura <kbjude@gmail.com>
2021-02-04 23:04:35 +0300

    Operate WIP

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,1 +1,24 @@
 import Big from 'big.js';
+const operate = (firstNumber, secondNumber, operation) => {
+    const firstNumber = Big(firstNumber);
+    const secondNumber = Big(secondNumber);
+    let total = 0;
+
+    switch (operation) {
+        case '+':
+            total = firstNumber.plus(secondNumber);
+            break;
+
+        case '-':
+            total = firstNumber.minus(secondNumber);
+            break;
+
+        case 'x':
+            total = firstNumber.times(secondNumber);
+            break;
+        case '-':
+            total = firstNumber.minus(secondNumber);
+            break;
+
+    }
+}
