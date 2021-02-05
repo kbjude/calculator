d0ad28496d94caa0f645fe934978ef341a505f3d
Jude Kajura <kbjude@gmail.com>
2021-02-06 02:59:48 +0300

    WIP app.js adding a class and state

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,32 @@
 import './App.css';
+import { Component } from 'react';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 import calculate from '../logic/calculate';
 
-
-export default class App extends React.Component {
-  constructor(props){
+export default class App extends Component {
+  constructor(props) {
     super(props);
 
-    this.state={
+    this.state = {
       total: null,
       next: null,
-      operation: null,
-    }
+      operation: null
+    };
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick(buttonName){
-    this.setState(state=>(state, buttonName))
+  handleClick(buttonName) {
+    this.setState(state => calculate(state, buttonName));
   }
 
   render() {
-    const {total, next} = this.state;
+    const { total, next } = this.state;
     return (
       <>
-        <Display result={next || total || '0'}/>
-        <ButtonPanel clickHandler={this.handleClick}/>
+        <Display result={next || total || '0'} />
+        <ButtonPanel clickHandler={this.handleClick} />
       </>
     );
   }
 }
-
-const data = {
-  total: 0,
-  next: 0,
-  operation: '',
-};
-
-calculate(data, '-');
