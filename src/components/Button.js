866a119a9f4761bb5712b2baf7edb0f38846eb64
Jude Kajura <kbjude@gmail.com>
2021-02-28 20:34:31 +0300

    Working buttons for calc

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,15 +2,32 @@ import PropTypes from 'prop-types';
 import './Button.css';
 
 const Button = props => {
-  const { name, clickHandler } = props;
+  const {
+    name, clickHandler, color, wide,
+  } = props;
   const handleClick = () => {
     clickHandler(name);
   };
   return (
-    <button type="button" className="button" onClick={handleClick}>
+    <button
+      type="button"
+      onClick={handleClick}
+      className={`calculator-key ${wide} ${color} button`}
+    >
       {name}
     </button>
   );
 };
-Button.propTypes = { name: PropTypes.string.isRequired, clickHandler: PropTypes.func.isRequired };
+Button.propTypes = {
+  name: PropTypes.string,
+  clickHandler: PropTypes.func.isRequired,
+  color: PropTypes.string,
+  wide: PropTypes.bool,
+};
+
+Button.defaultProps = {
+  name: '',
+  color: 'orange',
+  wide: false,
+};
 export default Button;
