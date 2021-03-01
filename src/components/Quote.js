bd48c42de8d62196f9740a4a6dd4d9c1ac0a4a46
Jude Kajura <kbjude@gmail.com>
2021-03-02 02:02:38 +0300

    Working calc

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,15 +1,14 @@
-import React from 'react'
+import React from 'react';
 
-const Quote = () => {
-    return (
-        <div>
-            <h1> Quote of the Day</h1>
-            <p>
-            "Imagination is more important than knowledge. For knowledge is limited, whereas imagination embraces the entire 
-            world, stimulating progress, giving birth to evolution."
-            </p>
-        </div>
-    )
-}
+const Quote = () => (
+  <div>
+    <h1> Quote of the Day</h1>
+    <p>
+      Imagination is more important than knowledge. For knowledge is limited, whereas
+      imagination embraces the entire
+      world, stimulating progress, giving birth to evolution.
+    </p>
+  </div>
+);
 
-export default Quote
+export default Quote;
