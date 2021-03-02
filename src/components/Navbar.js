70227c2ae15090ba291ab20f6a5f0d5ee4d12d8c
Jude Kajura <kbjude@gmail.com>
2021-03-03 02:06:47 +0300

    Adjusting the panel

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => (
-  <div>
+  <div className="nav-bar">
     <Link to="/">Home</Link>
     <Link to="/Quote">Quote</Link>
     <Link to="/App">Calculator</Link>
