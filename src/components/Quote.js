24ba955440da043d23c444dcbded8cecc3fa72ee
Jude Kajura <kbjude@gmail.com>
2021-03-05 21:43:47 +0300

    Adding random quotes

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -2,14 +2,18 @@ import React, { useState, useEffect } from 'react';
 import quotes from '../Quotes.json';
 
 const Quote = () => {
-  const randomNumber = Math.round(Math.random() * 20);
+  const randomNumber = Math.round(Math.random() * 1);
   const [quote, setQuote] = useState('Beautiful quotes');
   useEffect(() => {
     setQuote(quotes[randomNumber]);
   }, []);
   return (
     <>
-      <div data-testid="quote" className="quote">{quote}</div>
+      <div data-testid="quote">
+        {' '}
+        { quote }
+        {' '}
+      </div>
     </>
   );
 };
